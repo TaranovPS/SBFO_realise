fe0a965d9384c93f22c3c24d72fe6d353f37394f
taran <taranovpavel00@mail.ru>
2025-04-16 15:03:33 +0300

    comit

--- src/main/java/ru/taranov/dev/security/SecurityConfig.java
+++ src/main/java/ru/taranov/dev/security/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**").permitAll()
+                        .requestMatchers("/login", "/css/**", "/startPage").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
